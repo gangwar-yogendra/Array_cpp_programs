4bca7c3703c605b59fb20d8f58c5392c6ef8ecac
gangwar-yogendra <31799734+gangwar-yogendra@users.noreply.github.com>
2020-04-18 10:00:17 +0530

    Update PairWithSumX.cpp

--- PairWithSumX.cpp
+++ PairWithSumX.cpp
@@ -1,9 +1,25 @@
-/**
-    * Write a program that, given an array Arr[] of n numbers and another number x, 
-    * determines whether or not there exist two elements in S whose sum is exactly x.
-    * 1. With complexity O(nlogn)
-    * 2. With complexity O(n)
-*/
+/**    
+Given an array A[] and a number x, check for pair in A[] with sum as x
+
+Write a program that, given an array A[] of n numbers and another number x, 
+determines whether or not there exist two elements in S whose sum is exactly x.
+Examples:
+
+Input: arr[] = {0, -1, 2, -3, 1}
+        sum = -2
+Output: -3 ,1
+Explanation: If we calculate the sum,
+1 + (-3) = -2
+
+Input: arr[] = {1, -2, 1, 0, 5}
+       sum = 0
+Output: -1
+Explanation: No valid pair exists
+
+Solution:
+    1. With complexity O(nlogn)
+    2. With complexity O(n)
+**/
 
 #include <iostream>
 #include <algorithm>
