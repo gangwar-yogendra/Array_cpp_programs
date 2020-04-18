c40383b24a48118e5f0e3a24fbf92033262152fe
gangwar-yogendra <31799734+gangwar-yogendra@users.noreply.github.com>
2020-04-18 10:05:39 +0530

    Count Inversions in an array | Set 1 (Using Merge Sort)

--- inversionCount.cpp
+++ inversionCount.cpp
@@ -1,4 +1,4 @@
-/* 
+/*  
 Count Inversions in an array (Using Merge Sort)
 
 Inversion Count for an array indicates – how far (or close) the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum.
